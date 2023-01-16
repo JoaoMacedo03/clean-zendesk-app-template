2b0794d710b8980debbe2c370590576ce13cbc40
Joao Macedo <joaovictormacedo91@gmail.com>
2023-01-16 20:09:42 -0300

    test: ensure GithubUser throws GithubUserError on HttpClient failure

--- tests/data/use-cases/get-github-user-data/get-github-user-data.spec.ts
+++ tests/data/use-cases/get-github-user-data/get-github-user-data.spec.ts
@@ -1,31 +1,42 @@
 import faker from 'faker'
 import { GitHubUserModel } from '@/domain/models'
 import { GetGithubUserData } from '@/data/use-cases/get-github-user-data'
-import { HttpClientSpy } from '../../test'
+import { HttpClientSpy } from '@/tests/data/test'
+import { HttpStatusCode } from '@/data/contracts/http'
+import { GithubUserError } from '@/domain/errors'
 
 type GithubUserDataTypes = {
     githubUserData: GetGithubUserData
+    githubUser: string
     httpClientSpy: HttpClientSpy<GitHubUserModel>
 }
 
-const makeGithubUserData = (url: string): GithubUserDataTypes => {
+const makeGithubUserData = (url: string = faker.internet.url()): GithubUserDataTypes => {
     const httpClientSpy = new HttpClientSpy<GitHubUserModel>()
     const githubUserData = new GetGithubUserData(url, httpClientSpy)
+    const githubUser = faker.random.word()
     return {
         githubUserData,
+        githubUser,
         httpClientSpy
     }
 }
 
 describe('GetGithubUserData', () => {
     test('Should call HttpClient with correct values', async () => {
-        const githubUser = faker.random.word()
         const url = faker.internet.url()
-
-        const { githubUserData, httpClientSpy } = makeGithubUserData(url)
+        const { githubUserData, httpClientSpy, githubUser } = makeGithubUserData(url)
         await githubUserData.get(githubUser)
-
         expect(httpClientSpy.url).toBe(`${url}/${githubUser}`)
         expect(httpClientSpy.method).toBe('GET')
+    })
+
+    test('Should throw GithubUserError on HttpClient failure', async () => {
+        const { githubUserData, httpClientSpy, githubUser } = makeGithubUserData()
+        httpClientSpy.response = {
+            statusCode: HttpStatusCode.forbidden
+        }
+        const promise = githubUserData.get(githubUser)
+        await expect(promise).rejects.toThrow(new GithubUserError())
     })
 })
