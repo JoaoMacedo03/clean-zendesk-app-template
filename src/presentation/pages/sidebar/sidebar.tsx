42fc9cb63ae34bd3858e1965856e3793d9a8703f
Joao Macedo <joaovictormacedo91@gmail.com>
2023-01-16 18:49:55 -0300

    feat: add sidebar component

--- src/presentation/pages/sidebar/sidebar.tsx
+++ src/presentation/pages/sidebar/sidebar.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Button, TextField } from '@mui/material'
 import Styles from './sidebar-styles.scss'
 
 const Sidebar: React.FC = () => {
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+        event.preventDefault()
+        console.log('Test Submit')
+    }
+
     return (
-        <div>Oi eu sou um sidebar</div>
+        <form onSubmit={handleSubmit} className={Styles.sidebarWrap}>
+            <TextField
+                variant="outlined"
+                label="Oisfsdfs"
+                fullWidth
+            />
+            <Button type="submit" variant="contained">Buscar</Button>
+        </form>
     )
 }
 
