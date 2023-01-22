1d0a41fc6fc54646c1a300ce709bd56c5887048a
Joao Macedo <joaovictormacedo91@gmail.com>
2023-01-22 18:03:35 -0300

    feat: add portuguese texts

--- src/presentation/translations/locales/pt/translation.ts
+++ src/presentation/translations/locales/pt/translation.ts
@@ -1,9 +1,40 @@
 export const portuguese = {
+    "domain": {
+        "errors": {
+            "github-user-error": "Não foi possível buscar os dados desse usuário no github",
+            "github-user-repos-error": "Não foi possível buscar os repositórios desse usuário no github",
+            "unexpected-error": "Algo deu errado. Tente novamente em breve."
+        }
+    },
+    "infra": {
+        "http": {
+            "server-error": "Erro interno servidor"
+        }
+    },
     "presentation": {
         "pages": {
             "sidebar": {
-                "button": "Buscar"
+                "button": "Buscar",
+                "label": "Informe o nome do usuário GitHub",
+                "components": {
+                    "github-user-data": {
+                        "success": "Dados enviados ao ticket!",
+                        "goback-button": "Voltar",
+                        "followers": "Seguidores",
+                        "following": "Seguindo",
+                        "name": "Nome",
+                        "city": "Cidade"
+                    }
+                }
             }
+        }
+    },
+    "validation": {
+        "errors": {
+            "required-field-error": "Campo obrigatório",
+            "min-length-error-1": "Campo deve ter no mínimo ",
+            "min-length-error-2": " caracteres",
+            "invalid-field-error": "Valor inválido"
         }
     }
 }
