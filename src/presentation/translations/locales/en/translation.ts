1e8a6f541488f857b3a3312c4efe794d374b0f67
Joao Macedo <joaovictormacedo91@gmail.com>
2023-01-22 18:12:39 -0300

    feat: add english texts

--- src/presentation/translations/locales/en/translation.ts
+++ src/presentation/translations/locales/en/translation.ts
@@ -1,9 +1,40 @@
 export const english = {
+    "domain": {
+        "errors": {
+            "github-user-error": "Unable to fetch user data from github",
+            "github-user-repos-error": "Unable to fetch user repositories data from github",
+            "unexpected-error": "Something went wrong. Try again soon."
+        }
+    },
+    "infra": {
+        "http": {
+            "server-error": "Internal server error"
+        }
+    },
     "presentation": {
         "pages": {
             "sidebar": {
-                "button": "Search"
+                "button": "Search",
+                "label": "Enter GitHub username",
+                "components": {
+                    "github-user-data": {
+                        "success": "Data sent to ticket!",
+                        "goback-button": "Go Back",
+                        "followers": "Followers",
+                        "following": "Following",
+                        "name": "Name",
+                        "city": "City"
+                    }
+                }
             }
+        }
+    },
+    "validation": {
+        "errors": {
+            "required-field-error": "Required field",
+            "min-length-error-1": "Field must have at least ",
+            "min-length-error-2": " characters",
+            "invalid-field-error": "Invalid value"
         }
     }
 }
