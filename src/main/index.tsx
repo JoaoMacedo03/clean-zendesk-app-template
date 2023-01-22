e7e04c440160dd18a46000404abca91b2aea02b3
Joao Macedo <joaovictormacedo91@gmail.com>
2023-01-22 16:29:37 -0300

    feat: add i18next main file

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import Router from '@/main/routes/router'
+import { CircularProgress } from '@mui/material'
 import '@/presentation/styles/global.scss'
+import '@/presentation/translations'
 
 const container = document.getElementById('main')
 const root = createRoot(container)
 
-root.render(<Router />)
+root.render(
+    <Suspense fallback={<CircularProgress />}>
+        <Router />
+    </Suspense>
+)
---
+++ src/presentation/translations/index.ts
@@ -0,0 +1,21 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-xhr-backend';
+
+i18n
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    debug: false,
+    fallbackLng: 'pt',
+    react: {
+      useSuspense: false,
+    },
+    resources: {
+      en: {
+        translation: 'Hello World',
+      }
+    },
+  });
+
+export default i18n;
